7128c2eb47719310c9faaf3d9e14b186280b46e5
Guilluuu <guillegafc@gmail.com>
2024-10-27 23:52:03 +0100

    Autoupdated

--- autoupdate.sh
+++ autoupdate.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-while true; do
     git add .
     git commit -m "Autoupdated"
     git push
     echo "Actualización automática completada."
-    sleep 60  # Esperar 60 segundos antes de volver a ejecutar
-done
